9b6a74915fd4489c6ffb42399c81676049d2963e
Xelac <68864369+Xelacnnister@users.noreply.github.com>
2021-10-25 18:07:53 +0200

    Nivell 1 - Ex. 1

--- Sprint-5-Acudits/index.js
+++ Sprint-5-Acudits/index.js
@@ -1,2 +1,44 @@
 "use strict";
-console.log('Hello from TypeScript');
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var _a;
+// Acces the jokeButton & adds an event listener to it to execute the getJoke()
+(_a = document.querySelector("#jokeButton")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", getJoke);
+// Declare & initiate const jokeApiUrl to store the api url
+const jokeApiUrl = "https://icanhazdadjoke.com/";
+//Declare async function getJoke() and type it as Promise<JokeResponse>
+function getJoke() {
+    return __awaiter(this, void 0, void 0, function* () {
+        //Declare & initiate let respData so we can return it at the end of the function to avoid errors betwen the type of the function and the value returned
+        let respData = {
+            id: "string",
+            joke: "string",
+            status: 0
+        };
+        //fetch to the API. Use the jokeApiUrl to get the url. Apply the GET method & the headers to acces the API data
+        yield fetch(jokeApiUrl, {
+            method: "GET",
+            headers: { 'Accept': 'application/json' }
+        })
+            //Transform the string response to json format, so it becomes readable
+            .then((resp) => resp.json())
+            .then((data) => {
+            respData = data;
+            // Declare the const response to store the result of getJoke(). Assign data.joke to it
+            const response = data.joke;
+            //log the result of response
+            console.log(response);
+            //return the data to avoid errors with the type
+            return data;
+        });
+        //return the data to avoid errors with the type
+        return respData;
+    });
+}
--- Sprint-5-Acudits/index.ts
+++ Sprint-5-Acudits/index.ts
@@ -1,1 +1,39 @@
-console.log('Hello from TypeScript'); 
+//API data response interface
+interface JokeResponse {
+    id: string;
+    joke: string;
+    status: number
+}
+
+// Acces the jokeButton & adds an event listener to it to execute the getJoke()
+document.querySelector("#jokeButton")?.addEventListener("click", getJoke);
+
+// Declare & initiate const jokeApiUrl to store the api url
+const jokeApiUrl: string = "https://icanhazdadjoke.com/";
+
+//Declare async function getJoke() and type it as Promise<JokeResponse>
+async function getJoke(): Promise<JokeResponse>{
+    //Declare & initiate let respData so we can return it at the end of the function to avoid errors betwen the type of the function and the value returned
+    let respData: JokeResponse = {
+        id: "string",
+        joke: "string",
+        status: 0
+    };
+    //fetch to the API. Use the jokeApiUrl to get the url. Apply the GET method & the headers to acces the API data
+    await fetch(jokeApiUrl, { 
+        method: "GET",
+        headers: {'Accept': 'application/json'} })
+        //Transform the string response to json format, so it becomes readable
+        .then((resp): Promise<JokeResponse> => resp.json())
+        .then((data): JokeResponse  => {
+            respData = data;
+            // Declare the const response to store the result of getJoke(). Assign data.joke to it
+            const response = data.joke;
+            //log the result of response
+            console.log(response);
+            //return the data to avoid errors with the type
+            return data
+        })
+        //return the data to avoid errors with the type
+        return respData
+}
